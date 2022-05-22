dfef7b20e7b1559f87250076e36c7a8e883cc8a1
vela <47091951+moondisorder@users.noreply.github.com>
2022-05-22 12:12:02 +0930

    True or False Drinking question

--- py/udemy/UdemyPython01.py
+++ py/udemy/UdemyPython01.py
@@ -1,6 +1,25 @@
 
+##### TRUE OR FALSE TEST #####################
+##boobquestion = input("Do you have boobs?")
+#if boobquestion=="yes":
+#    print("I have boobs. Oppai!")
+#else: 
+#    print("I dont have boobs. Flat justice.❤️‍🔥")
 
-print(bool(0))
+### True or False ID showing question.###################
+#age = int(input('''
+#How old are you? 
+#Show me your ID!'''))
+#age = int(age)
+
+#age>=21
+
+#if age>=21:
+#    print('''Come on in and drink!
+#    🍷🍸🍹🍺🍻🍾''')
+#else: 
+#    print("Go home kid!😠")
+
 
 
 
