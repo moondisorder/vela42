f29e8f7f73a4ce371d6e6b370db67943c827a32c
vela <47091951+moondisorder@users.noreply.github.com>
2022-05-24 13:33:46 +0930

    toothpick game attempt

--- py/udemy/toothpick_game_attempt.py
+++ py/udemy/toothpick_game_attempt.py
@@ -3,19 +3,18 @@
 num_left = 13
 player1_name = input("Input player1 name!")
 player2_name = input("Input player2 name!")
+current_player=player1_name
 
 while True:
     print("|"*num_left)
     print(f"There are {num_left} toothpicks left")
-    p1_choice=int(input(f"{player1_name}, how many toothpicks do you take?"))
-    num_left-=p1_choice
-    if num_left <= 0:
-        print(f"{player1_name} wins the game!")
+    choice = int(input(f"{current_player}, how many toothpicks do you take?"))
+    num_left -= choice
+    if num_left <=0:
+        print(f"{current_player} wins the game!")
         break
-
-    p2_choice=int(input(f"{player2_name}, how many toothpicks do you take?"))
-    num_left-=p2_choice
-    if num_left <= 0:
-        print(f"{player2_name} wins the game!")
-        break
+    if current_player == player1_name:
+        current_player = player2_name
+    else:
+        current_player = player1_name
 print("GAME OVER!")
