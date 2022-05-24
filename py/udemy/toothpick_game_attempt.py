6543c11a7584b7a516b59073e84ee2d1060480f2
vela <47091951+moondisorder@users.noreply.github.com>
2022-05-24 13:37:25 +0930

    Update toothpick game

--- py/udemy/toothpick_game_attempt.py
+++ py/udemy/toothpick_game_attempt.py
@@ -9,6 +9,8 @@ while True:
     print("|"*num_left)
     print(f"There are {num_left} toothpicks left")
     choice = int(input(f"{current_player}, how many toothpicks do you take?"))
+    while choice !=1 and choice !=2 and choice !=3:
+        choice = int(input(f"You can only choose 1, 2 or 3!"))
     num_left -= choice
     if num_left <=0:
         print(f"{current_player} wins the game!")
